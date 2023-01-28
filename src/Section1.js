d3e99b3fbd0453193ebcd23064b8743ffac84d93
Tarun <Tarunjoshiji1@gmail.com>
2023-01-28 17:03:47 +0530

    react learning

---
+++ src/Section1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Section1 extends Component {
+  render() {
+    return (
+      <div>Section1</div>
+    )
+  }
+}
---
+++ src/Section2.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Section2 extends Component {
+  render() {
+    return (
+      <div>Section2</div>
+    )
+  }
+}
