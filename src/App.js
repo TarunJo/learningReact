9187f18989b9fecdaf5bbe6c55b54d493c6399fe
Tarun <Tarunjoshiji1@gmail.com>
2023-01-29 16:21:37 +0530

    1.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { Component } from 'react';
 import './App.css';
-import Section1 from './Section1';
-import Section2 from './Section2';
+import {Section1, Section2, Heading} from './components';
 
 function App() {
   return (
     <div className="App">
+      <Heading/>
       <Section1/>
       <Section2/>
     </div>
